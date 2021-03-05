2f9e9425b9da2663d5c4df397b4eb1e755220d95
Bringan <72128044+Bringann@users.noreply.github.com>
2021-03-05 10:51:09 +0300

    Add files via upload

---
+++ Compute_quotient_and_remainder.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "rus");
+
+    int dividend, divisor, quotient, remainder;
+
+    cout << "Введите делимое: ";
+    cin >> dividend;
+    cout << "Введите делитель: ";
+    cin >> divisor;
+
+    quotient = dividend / divisor;
+    remainder = dividend % divisor;
+
+    cout << "Частное: " << quotient << endl;
+    cout << "Остаток: " << remainder << endl;
+
+    return 0;
+}
