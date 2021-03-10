96680d6e2bb5cfff6337cb0d6d232643cd6848fa
Bringan <72128044+Bringann@users.noreply.github.com>
2021-03-10 16:06:14 +0300

    Add files via upload

---
+++ Even_or_odd.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+
+    int n;
+    cout << "Введите целое число: ";
+    cin >> n;
+
+    (n % 2 == 0) ? cout << n << " четное число." : cout << n << " нечетное число.";
+        
+
+    return 0;
+}
