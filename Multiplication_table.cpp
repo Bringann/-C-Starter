ca5b699177555773f8b9bb3849d46d43750be982
Bringan <72128044+Bringann@users.noreply.github.com>
2021-03-12 18:57:07 +0300

    Add files via upload

---
+++ Multiplication_table.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+
+    cout << "Таблица умножения:\n";
+
+    for (int i = 1; i <= 10; i++)
+    {
+        for (int j = 1; j <= 10; j++)
+        {
+            cout << setw(2) << j * i << " ";
+        }
+        cout << "\n";
+    }
+
+    return 0;
+}
