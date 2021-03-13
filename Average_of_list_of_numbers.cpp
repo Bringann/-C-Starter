0afaa930e2f154320b92465258b8e3f87cd1c9b8
Bringan <72128044+Bringann@users.noreply.github.com>
2021-03-13 20:13:20 +0300

    Add files via upload

---
+++ Average_of_list_of_numbers.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "russian");
+
+    double n;
+    double avr;
+    double sum = 0;
+
+    cout << "Количество чисел: ";
+    cin >> n;
+
+    for (int i = 1; i <= n; i++)
+    {
+        cout << "\nВведите число: " << i << ": ";
+        cin >> avr;
+        sum += avr;
+    }
+
+    avr = sum / n;
+
+    cout << "\nСреднее арифметическое: " << avr << "\n";
+
+    return 0;
+}
