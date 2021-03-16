9d02abb686e426dcd7535466805f78fa66b91dd2
Bringan <72128044+Bringann@users.noreply.github.com>
2021-03-16 19:10:22 +0300

    Add files via upload

---
+++ Fibonacci.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "russian");
+    int n;
+    long first = 0;
+    long second = 1;
+    long sum = 0;
+
+    cout << "Введите количество элементов: ";
+    cin >> n;
+
+    cout << "Последовательность чисел Фибоначчи: " << first << " " << second;
+
+    for (int i = 2; i < n; ++i)
+    {
+        sum = first + second;
+        first = second;
+        second = sum;
+        cout << " " << sum;
+    }
+
+    return 0;
+}
