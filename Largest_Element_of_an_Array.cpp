1719ddb4120e47514b2b5514979c3be3b132ba43
Bringan <72128044+Bringann@users.noreply.github.com>
2021-03-18 17:13:08 +0300

    Add files via upload

---
+++ Largest_Element_of_an_Array.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "russian");
+    int i, n;
+    float arr[100];
+
+    cout << "Введите размер массива(от 1 до 100): ";
+    cin >> n;
+    cout << endl;
+
+    for (i = 0; i < n; ++i)
+    {
+        cout << "Введите число " << i + 1 << " : ";
+        cin >> arr[i];
+    }
+
+    for (i = 1; i < n; ++i)
+    {
+        if (arr[0] < arr[i])
+            arr[0] = arr[i];
+    }
+
+    cout << "Самое большое число = " << arr[0];
+
+    return 0;
+}
