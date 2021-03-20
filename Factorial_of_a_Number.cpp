0bd97e9523d2b1406545d8ff6334b9d6dc569649
Bringan <72128044+Bringann@users.noreply.github.com>
2021-03-20 16:29:34 +0300

    Add files via upload

---
+++ Factorial_of_a_Number.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "russian");
+    int n;
+    unsigned long long fact = 1;
+
+    cout << "Введите целое положительное число: ";
+    cin >> n;
+
+    if (n < 0)
+    {
+        cout << "Ошибка! Факториала отрицательного числа не существует.";
+    }
+    else
+    {
+        for (int i = 1; i <= n; ++i)
+        {
+            fact *= i;
+        }
+        cout << "Факториал числа: " << n << " = " << fact;
+    }
+
+    return 0;
+}
