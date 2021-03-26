48b029c1c85c1b065c55a3d2787b51a56292b0fb
Bringan <72128044+Bringann@users.noreply.github.com>
2021-03-26 19:16:17 +0300

    Add files via upload

---
+++ Exponentiation.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    //double exponent = 0;
+    int exponent = 0;
+    double base = 0;
+    double result = 1;
+
+    cout << "Введите число: ";
+    cin >> base;
+    cout << "Введите степень: ";
+    cin >> exponent;
+
+    cout << base << " ^ " << exponent << " = ";
+
+    while (exponent != 0)
+    {
+        result *= base;
+        --exponent;
+    }
+
+    //result = pow(base, exponent);
+
+    cout << result;
+
+    return 0;
+}
